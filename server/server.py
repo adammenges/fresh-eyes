a29fd5a879ddc060d289e5b193749a9a58a5985e
Adam Menges <adam@adammenges.com>
2018-01-26 20:43:50 -0800

    add imports

--- server/server.py
+++ server/server.py
@@ -1,4 +1,20 @@
 from flask import Flask
+from collections import defaultdict, OrderedDict
+import time
+from functools import (reduce, wraps)
+import re
+import os
+import logging
+from threading import RLock
+
+# ML
+import tensorflow as tf
+from keras.backend import learning_phase
+from keras.losses import get as get_keras_loss
+from keras.metrics import categorical_accuracy
+from keras.layers import Input
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route("/getPrediction")
