2138df9b555dba78430b8f9df290daeac69dbbb8
Kyle Steinfeld <ksteinfe@gmail.com>
2018-03-09 14:45:17 -0800

    added time notation to file_opener

--- rhinoscripts/file_opener.py
+++ rhinoscripts/file_opener.py
@@ -1,14 +1,18 @@
 import rhinoscriptsyntax as rs
-import os
+import os, time
 
 basepath = "W:\\Box Sync\\Berkeley\\Courses Active\\100d s18\\10-15-100 Houses\\10-15-100 Massings"
-folderpath = os.path.join(basepath,"XX - Test")
+folderpath = os.path.join(basepath,"AA - Cylindrical")
 
+folder_tic = time.clock()
 for filename in os.listdir(folderpath):
     if filename.endswith(".3dm"):
+        file_tic = time.clock()
         filepath = os.path.join(folderpath, filename)
-        print(filepath)
         rs.DocumentModified(False)
         rs.Command('_-Open {} _Enter'.format('"'+filepath+'"'))
-        
+        t = round(time.clock()-file_tic)
+        print(filename+"\ttime:\t"+str(t))
 
+t = round(time.clock()-folder_tic)
+print("TOTAL\ttime:\t"+str(t))
